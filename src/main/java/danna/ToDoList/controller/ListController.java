453601a1446af75e0c4f948b31ba46ffdce1b06d
SamuelMoniz <samuel324moniz@gmail.com>
2025-09-11 22:24:26 +0200

    Mostrar listas

--- src/main/java/danna/ToDoList/controller/ListController.java
+++ src/main/java/danna/ToDoList/controller/ListController.java
@@ -1,26 +1,23 @@
 package danna.ToDoList.controller;
 
 import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import danna.ToDoList.model.ListEntity;
+import danna.ToDoList.dto.ListDto;
 import danna.ToDoList.service.ListService;
 
 @RestController
-@RequestMapping("api/lists")
+@RequestMapping("/api/lists")
 public class ListController {
     private final ListService listService;
 
-    public ListController (ListService listService) {
+    public ListController(ListService listService) {
         this.listService = listService;
     }
 
-    // Post de todas las listas 
-    @GetMapping()
-    public List<ListEntity> getAllLists() {
+    @GetMapping
+    public List<ListDto> getAllLists() {
         return listService.getAllLists();
     }
 }
